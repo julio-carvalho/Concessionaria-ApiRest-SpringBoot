4f4c9f982a0357cf890c27e672217ebc10c8ee30
Julio Carvalho <jcarvalhok3@gmail.com>
2022-03-04 11:17:19 -0300

    spring security I

--- src/main/java/br/com/concessionaria/config/SpringFoxConfig.java
+++ src/main/java/br/com/concessionaria/config/SpringFoxConfig.java
@@ -1,4 +1,4 @@
-package br.com.concessionaria;
+package br.com.concessionaria.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
